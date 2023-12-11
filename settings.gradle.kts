59e1e5be1997319dc9232fc8678ae070a93719a1
Paguro <ivankosaev2006@gmail.com>
2023-12-11 13:16:39 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "electrical_diagram_mobile"
+include(":app")
