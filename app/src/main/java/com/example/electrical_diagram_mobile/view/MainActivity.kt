34ce021835699417f24174145e849e9c1c444a39
Paguro <ivankosaev2006@gmail.com>
2023-12-16 21:16:40 +0300

    Add basic navigation

--- app/src/main/java/com/example/electrical_diagram_mobile/view/MainActivity.kt
+++ app/src/main/java/com/example/electrical_diagram_mobile/view/MainActivity.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.Row
-
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -65,79 +63,143 @@ import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     //объект схемы
-    var diagram: Diagram? = null
-    @OptIn(ExperimentalMaterial3Api::class)
+    private var diagram: Diagram = Diagram("", 0, 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Electrical_diagram_mobileTheme {
-                // открытие окна создания схемы
-                val openDialog = remember { mutableStateOf(true) }
-                if(openDialog.value){
-                    var name by remember{mutableStateOf("")}
-                    var x by remember{mutableStateOf("")}
-                    var y by remember{mutableStateOf("")}
-                    AlertDialog(
-                        onDismissRequest = {
-                        },
-                        title = {
-                            Row(
-                                Modifier.fillMaxWidth(),
-                                Arrangement.Center
-                            ){
-                                Text("Создать схему")
-                            }},
-                        text = {
-                            Column(){
-                                TextField(
-                                    value = name,
-                                    onValueChange = {name = it},
-                                    label={Text("Название")},
-                                    singleLine = true,
-                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                                    modifier = Modifier.padding(10.dp))
-                                Row(Modifier.fillMaxWidth(),
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                    verticalAlignment = Alignment.CenterVertically) {
-                                    TextField(
-                                        value = x,
-                                        onValueChange = {x = it},
-                                        label={Text("Ширина")},
-                                        singleLine = true,
-                                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                        modifier = Modifier.weight(1f).padding(10.dp))
-                                    TextField(
-                                        value = y,
-                                        onValueChange = {y = it},
-                                        label={Text("Длина")},
-                                        singleLine = true,
-                                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                        modifier = Modifier.weight(1f).padding(10.dp)
-                                    )
-                                }
-                            }},
-                        confirmButton = {
-                            Row(
-                                Modifier.fillMaxWidth(),
-                                Arrangement.Center
-                            ){
-                                TextButton(
-                                    onClick = {
-                                        //создание объекта схемы
-                                        try {
-                                            diagram = Diagram(name, x.toInt(), y.toInt())
-                                            openDialog.value = false
-                                        }catch (_: Exception){ }
-                                    }) {
-                                    Text("Создать")
-                                }
-                            }
+                // открытие диалога создания схемы
+                DialogCreateDiagram()
+            }
+        }
+    }
+
+    // диалог создания схемы
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun DialogCreateDiagram(){
+        val openDialog = remember { mutableStateOf(true) }
+        if (openDialog.value) {
+            var name by remember { mutableStateOf("") }
+            var x by remember { mutableStateOf("") }
+            var y by remember { mutableStateOf("") }
+            AlertDialog(
+                onDismissRequest = {
+                },
+                title = {
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        Arrangement.Center
+                    ) {
+                        Text("Создать схему")
+                    }
+                },
+                text = {
+                    // поля ввода данных
+                    Column() {
+                        TextField(
+                            value = name,
+                            onValueChange = { name = it },
+                            label = { Text("Название") },
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                            modifier = Modifier.padding(10.dp)
+                        )
+                        Row(
+                            Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            TextField(
+                                value = x,
+                                onValueChange = { x = it },
+                                label = { Text("Ширина") },
+                                singleLine = true,
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(10.dp)
+                            )
+                            TextField(
+                                value = y,
+                                onValueChange = { y = it },
+                                label = { Text("Длина") },
+                                singleLine = true,
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(10.dp)
+                            )
                         }
-                    )}
+                    }
+                },
+                confirmButton = {
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        Arrangement.Center
+                    ) {
+                        TextButton(
+                            onClick = {
+                                //создание объекта схемы
+                                try {
+                                    diagram = Diagram(name, x.toInt(), y.toInt())
+                                    openDialog.value = false
+                                } catch (_: Exception) {
+                                }
+                            }) {
+                            Text("Создать")
+                        }
+                    }
+                }
+            )
+        }
+        else{
+            // доступ к навигации по схеме
+            DiagramView()
+        }
+    }
+
+    // схема
+    @Composable
+    fun DiagramView() {
+        // навигация
+        var zoomState by remember { mutableStateOf(1f) }
+        var rotationState by remember { mutableStateOf(0f) }
+        var offsetState by remember { mutableStateOf(Offset(0f, 0f)) }
+        Box(
+            Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTransformGestures { _, pan, zoom, rotation ->
+                        // ограничение приближения
+                        zoomState = (zoomState * zoom).coerceIn(1f, 10f)
+                        // ограничение поворота
+                        rotationState = (rotationState + rotation).coerceIn(-180f, 180f)
+                        // ограничение перемещения
+                        val radiansRotate = Math.toRadians(rotationState.toDouble())
+                        val x = ((diagram.getSizeX() * 5 * kotlin.math.abs(sin(radiansRotate.absoluteValue)) * zoomState)).toFloat()
+                        val y = ((diagram.getSizeY() * 5 * kotlin.math.abs(sin(radiansRotate.absoluteValue)) * zoomState)).toFloat()
+                        offsetState = Offset(
+                            (offsetState.x + pan.x).coerceIn(-x, x),
+                            (offsetState.y + pan.y).coerceIn(-y, y))
+                    }
+                }
+        ) {
+            Box(modifier = Modifier
+                .offset { IntOffset(offsetState.x.roundToInt(), offsetState.y.roundToInt()) }
+                .rotate(rotationState)
+                .scale(zoomState)
+                .width(diagram.getSizeX().dp * 10)
+                .height(diagram.getSizeY().dp * 10)
+                .background(Color.Gray)
+                .align(Alignment.Center)){
+
             }
         }
     }
 }
+
 
 
 
